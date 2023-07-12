5172411e78a7e51e212d4492195ea379114332d1
Joel Aguilar <joel3.1416@gmail.com>
2023-07-12 00:45:46 -0600

    fast translate react-Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Advice } from './advice';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'advice_gen_angular';
+export class AppComponent implements OnInit {
+
+  advice: Advice;
+
+  constructor() {
+    this.advice = {
+      id: -1,
+      advice: ""
+    }
+  }
+
+  ngOnInit(): void {
+    if (!this.advice || this.advice.id == -1) {
+      this.getAdvice()
+    }
+    console.log("me inicie")
+  }
+
+  getAdvice() {
+    console.log('initAdvice')
+    fetch('https://api.adviceslip.com/advice')
+      .then((response) => response.json())
+      .then((data) => {
+        let { slip } = data
+        this.advice = {
+          id: slip.id,
+          advice:slip.advice
+        }
+        return //setAdvice({ ...data })
+      })
+    //.then(() => {
+    //setLoad(true)
+    //});
+  }
 }
